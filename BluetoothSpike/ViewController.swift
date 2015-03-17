0acbf791f8a90cd81ce423694806f6dfa7719e1c
Daniel Coffman <dgcoffman@gmail.com>
2015-03-17 11:26:48 -0700

    Initial Commit

---
+++ BluetoothSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothSpike
+//
+//  Created by dcoffman on 3/17/15.
+//  Copyright (c) 2015 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
