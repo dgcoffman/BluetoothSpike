78bdd8528f9f1f791dd99f4aa2bc615d467bd943
mkulumadzi <evan.waters@gmail.com>
2015-03-17 18:54:42 -0700

    Outputting central manager state to UI

---
+++ BluetoothSpike/BluetoothTesting.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var text = "What is up"
+
+text += ", Yo"
--- BluetoothSpike/ViewController.swift
+++ BluetoothSpike/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate{
+    
+    @IBOutlet weak var StatusBox: UITextView!
+    
     var bluetoothReady = false;
     var centralManager:CBCentralManager!
     var connectedDevice:CBPeripheral! // ! means implicitly unwrapper optional
@@ -17,6 +20,9 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        StatusBox.text = "Loading..."
+        
         startUpCentralManager();
     }
 
@@ -25,22 +31,33 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     }
     
     
+    //Outputting to the console and to a text field in the UI
+    //Tried to simplify with a function below but it was crashing
     func centralManagerDidUpdateState(central: CBCentralManager!) {
+        
         println("checking state")
+        StatusBox.text = StatusBox.text + ("\nchecking state")
+        
         switch(central.state) {
             case .PoweredOff:
                 println("Device is powered off")
+                StatusBox.text = StatusBox.text + ("\nDevice is powered off")
             case .PoweredOn:
                 println("Device is powered on and ready")
+                StatusBox.text = StatusBox.text + ("\nDevice is powered on and ready")
                 bluetoothReady = true
             case .Resetting:
                 println("Device is currently resetting")
+                StatusBox.text = StatusBox.text + ("\nDevice is currently resetting")
             case .Unauthorized:
-                println("Device is not authored to use this bluethooth device")
+                println("Device is not authorized to use this bluethoth device")
+                StatusBox.text = StatusBox.text + ("\nDevice is not authoreized to use this bluetooth device")
             case .Unknown:
                 println("Corebluthooth BLE state is unknown")
+                StatusBox.text = StatusBox.text + ("\nCore Bluretooth BLE state is unknown")
             case .Unsupported:
                 println("This device is not supported")
+                StatusBox.text = StatusBox.text + ("\nThis device is not supported")
         }
         
         if bluetoothReady {
@@ -48,6 +65,11 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         }
     }
     
+// Tried using this function above but it was intermitantly crashing with an (lldb) message
+//    func refreshStatusBox(line: String) {
+//        StatusBox.text = StatusBox.text + "\n\(line)"
+//    }
+
     func discoverDevices() {
         println("Currently attempting to discover what devices are available")
         centralManager.scanForPeripheralsWithServices(nil, options: nil)
